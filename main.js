55f3f6a812252db236c86d8ac02501c878dbe529
Sarkis Aghazarian <sarkis_aghazarian@yahoo.com>
2018-09-14 19:56:19 -0700

    latest changes to About_Me

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 'use strict';
         
+        var points = 0;
         var userName = prompt('Welcome player! What is your name?');
-        console.log('What is your name?: ' + userName);
+        console.log('Player username is: ' + userName);
 
-        var driveCar = prompt('Can Sarkis drive a stick shift?');
+        var driveCar = prompt('Can Sarkis drive a manual car?');
         var driveCarLow = driveCar.toLowerCase();
 
-        if(driveCarLow === 'yes'){
-            alert('Great guess! I can indeed drive a stick shift.');
-        }else if(driveCarLow === 'no'){
-            alert('No, I\'m a very skilled driver.');
-        }else {
+            if(driveCarLow === 'yes'){
+            alert('Great guess! I can indeed drive a manual car.');
+            points++;
+        }
+            else if(driveCarLow === 'no'){
+            alert('No, I\'m indeed a very skilled driver.');
+        }
+            else {
             prompt('Please answer yes or no');
         }
-        //console.log('Player response is: ' + driveCarUp);
+        console.log('Player response is: ' + driveCarLow);
 
         var beets = prompt('Does Sarkis like beets?');
         var beetsLow = beets.toLowerCase();
         
-        if(beetsLow === 'yes'){
+            if(beetsLow === 'yes'){
             alert('No way, I hate them, they\'re gross');
-        }else if(beetsLow === 'no'){
+        }
+            else if(beetsLow === 'no'){
             alert('Exactly! I don\'t like them at all.');
-        }else {
+            points++;
+        }
+            else {
             prompt('Please answer yes or no');
         }
         console.log('Does Sarkis like beets?: ' + beetsLow);
@@ -30,11 +37,14 @@
         var college = prompt('Did Sarkis attend college?');
         var collegeLow = college.toLowerCase();
 
-        if(collegeLow === 'yes'){
+            if(collegeLow === 'yes'){
             alert('Yes I did: University of Washington. Good times!');
-        }else if(collegeLow === 'no'){
+            points++;
+        }
+            else if(collegeLow === 'no'){
             alert('No, I did attend. It was fun.') 
-        }else {
+        }
+            else {
             prompt('Please answer yes or no');
         }
         console.log('Did Sarkis attend college: ' + collegeLow);
@@ -42,23 +52,29 @@
         var soccer = prompt('Did Sarkis play soccer in high school?');
         var soccerLow = soccer.toLowerCase();
         
-        if(soccerUp === 'yes'){
+            if(soccerUp === 'yes'){
             alert('Yes of course I did. I love soccer! Who doesn\'t');
-        }else if(soccerLow === 'no'){
+            points++;
+        }
+            else if(soccerLow === 'no'){
             alert('No! I did play in high school');
-        }else {
+        }
+            else {
             prompt('Please answer yes or no');
         }
-        console.log('Did Sarkis play soccer in high school?: ' + soccerLow);
+        console.log('Player answer to if Sarkis played soccer in high school is: ' + soccerLow);
 
         var guitar = prompt('Does Sarkis play the guitar?');
         var guitarLow = guitar.toLowerCase();
 
-        if(guitarLow === 'yes'){
-            alert('Ha! I wish.');
-        }else if(guitarLow === 'no'){
+            if(guitarLow === 'yes'){
+            alert('Ha! I wish.That is one skill I would like to have someday');
+        }
+            else if(guitarLow === 'no'){
             alert('Sadly you\'re right');
-        }else {
+            points++;
+        }
+            else {
             prompt('Please answer yes or no');
         }
         console.log('Does Sarkis play the guitar?: ' + guitarLow);
@@ -66,57 +82,44 @@
 
         var turns = 0;
 
-        while(turns < 4) {
+        while(turns < 5) {
         var soccerGoals = prompt('How many goals did I score in high school?');
             if(soccerGoals = 3) {
             alert('You are correct!');
+            points = points + 4;
             break;
         }
 
             else if(soccerGoals < 3) {
             prompt('Too low, try again');
             turns++;
+            points = points + (4 -turns);
         }
 
             else(soccerGoals > 3) ;
             prompt('Too high, try again');
             turns++;
+            points = points + (4 -turns);
 
         }
 
-        var musicBands = ['Wu Tang Clan', 'NIN', 'Depeche Mode', 'Joy Division']
         var turns = 0;
+        var musicBands = ['NIN', 'Tool', 'Blur',
+        'Depeche Mode'];
+        var band;
 
-        while(turns < 6) {
-        var band = prompt('Name one of my favorite bands');
-            for(i = 0; i < musicBands.length; i++) {
-                if(band === musicBands[i]) {
-                    alert('Good guess! You are right!');
-                    alert(musicBand[i]);
-                    break;
-                }
-            break;
-            } 
+        while (turns < 7) { //loop for guessing game 
 
-            else if(band !== musicBands[i]) { 
-            var band = prompt('Please try again');
-            turns++;
-            }
-            
-            else if(band === musicBands[i]) {
-                alert('You got it!');
-                alert(musicBands[i]);
-                break;
-                }
+        band = prompt('Name one of my favorite bands');
+            for (i = 0; i < musicBands.length; i++) { //array loop
+                if (band === musicBands[i]) {
+                alert('Great guess! You are right');
+                alert('These are all the bands ' + musicBands.join(", "));
+                turns = 7;
             break;
             }
-
-            else {
-                alert(musicBands[i]);
-            }
-            
+        }
+        turns++;
         }
 
-
-
-    
+        alert('You scored ' + points);
